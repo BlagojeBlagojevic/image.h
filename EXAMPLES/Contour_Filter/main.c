56d6345c982aa895c40259ac13b62ea2632bd0a2
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-02-16 16:59:30 +0100

    Update main.c

--- EXAMPLES/Contour_Filter/main.c
+++ EXAMPLES/Contour_Filter/main.c
@@ -5,12 +5,12 @@
 
 int main(void){
 	
-	Image a = Image_Alloc_Name("12.jpg");
+	Image a = Image_Alloc_Name("5.jpg");
 	Image b = Image_Alloc(IMAGE_PARAMETARS(a));
 	Image_Black_White_Filtar(b,20);
-  b = Image_Applay_Kernel(a,KERNEL_EDGE,3,3);
-  Image_Black_White_Filtar(b,40);
-  Image_Invert(b);
+	Image_Applay_Kernel(b,a,KERNEL_EDGE,3,3);
+ 	Image_Black_White_Filtar(b,40);
+  	Image_Invert(b);
 	Image_Save(b,"saved.jpg");
 	printf("Nesto\n\n\n");
 	system("pause");
