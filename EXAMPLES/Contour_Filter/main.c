e0e6443913ae8bc1bbffae2ce110c7da5a1f7a99
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-02-19 10:45:18 +0100

    Update main.c

--- EXAMPLES/Contour_Filter/main.c
+++ EXAMPLES/Contour_Filter/main.c
@@ -1,18 +1,22 @@
 #define IMAGE_IMPLEMENTATION
-#include "image.h"
+#include"image.h"
 
 
 
-int main(void){
+int main(void) {
 	
-	Image a = Image_Alloc_Name("5.jpg");
-	Image b = Image_Alloc(IMAGE_PARAMETARS(a));
-	Image_Black_White_Filtar(b,20);
-	Image_Applay_Kernel(b,a,KERNEL_EDGE,3,3);
- 	Image_Black_White_Filtar(b,40);
-  	Image_Invert(b);
-	Image_Save(b,"saved.jpg");
-	printf("Nesto\n\n\n");
-	system("pause");
+	Image i = Image_Alloc_Name("12.jpg");
+	//Image i = Image_Alloc_Name("source.png");
+	Image a = Image_Alloc(IMAGE_PARAMETARS(i));
+	//Image b = Image_Alloc_Name("12.jpg");
+
+	//uint32_t *image = stbi_load("12.jpg",);	
+	float KERNEL_BL[]={0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,};
+	Image_Applay_Kernel(a,i,KERNEL_EDGE,3,3);
+	Image_Black_White_Filtar(a,5);
+	Image_Invert(a); 
+	//Image_Zero_Chanel(a,2);
+	Image_Save(a,"saved.jpg");
+
 	return 0;
-}
+	}
